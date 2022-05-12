c83beb0344aeb874c3b9daf30030c2221cdd1e14
Dominic Gannaway <dg@domgan.com>
2022-05-12 17:02:39 +0100

    Address feedback

--- packages/lexical/src/LexicalNode.js
+++ packages/lexical/src/LexicalNode.js
@@ -617,7 +617,7 @@ export class LexicalNode {
 
   static importJSON(
     // $FlowFixMe: TODO
-    serialzedNode: Object,
+    serializedNode: Object,
   ): LexicalNode {
     invariant(
       false,
